d035a580b2247291743a6c522366a542d9268931
A-Lazar-A <fub.lazar@mail.ru>
2021-05-02 12:55:12 +0300

    new start

---
+++ hello_world_application/_build/CMakeFiles/hello.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello.dir/home/nikita/A-Lazar-A/workspace/projects/lab03/formatter_ex_lib/formatter_ex.cpp.o"
+  "libhello.a"
+  "libhello.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
